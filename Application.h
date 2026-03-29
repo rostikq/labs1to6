5c1d155c2ad33bea624f980070baa208aa994552
rostikq2 <rostikcakeyankun@gmail.com>
2026-03-29 16:43:40 +0300

    Create Application class for general logics

---
+++ Application.h
@@ -0,0 +1,30 @@
+//
+// Created by user on 3/29/2026.
+//
+
+#ifndef LABS1TO6_APPLICATION_H
+#define LABS1TO6_APPLICATION_H
+#include <filesystem>
+#include <iostream>
+#include <vector>
+
+#include "Classroom.h"
+#include "Lecturer.h"
+
+using namespace std;
+using namespace std::filesystem;
+
+class Application {
+private:
+    vector<unique_ptr<Lecturer>> m_lecturers;
+    vector<unique_ptr<Course>> m_courses;
+    vector<unique_ptr<Classroom>> m_classrooms;
+public:
+    Application() {
+        cout << "University schedule app constructed";
+    }
+    ~Application();
+};
+
+
+#endif //LABS1TO6_APPLICATION_H
