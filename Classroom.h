b06ee5a84e1c228da30653d91e5965e5ffcc33f2
rostikq2 <rostikcakeyankun@gmail.com>
2026-03-29 19:35:29 +0300

    Add fstream operators for Classroom

--- Classroom.h
+++ Classroom.h
@@ -8,7 +8,7 @@
 
 #include "ISchedulable.h"
 
-enum ClassroomType {
+enum ClassroomType : unsigned int {
     LECTURE_CLASSROOM,
     PRACTICE_CLASSROOM
 };
@@ -22,13 +22,13 @@ protected:
     ClassroomType m_roomType;
 
 public:
-    Classroom(): Classroom(0, 30) {}
+    Classroom(): Classroom(0, 30, LECTURE_CLASSROOM) {}
 
-    Classroom(unsigned int number, unsigned int capacity):
-    m_number(number), m_capacity(capacity){
+    Classroom(unsigned int number, unsigned int capacity, ClassroomType type):
+    m_number(number), m_capacity(capacity), m_roomType(type){
         m_count++;
     }
-    Classroom(const Classroom& other) : Classroom(other.m_number, other.m_capacity) {
+    Classroom(const Classroom& other) : Classroom(other.m_number, other.m_capacity, other.m_roomType) {
     }
     Classroom(Classroom&& other) noexcept {
         this->m_number = other.m_number;
@@ -46,12 +46,19 @@ public:
     unsigned int getCapacity() const {
         return m_capacity;
     }
+    ClassroomType getClassroomType() const {
+        return m_roomType;
+    }
 
     void setNumber(unsigned int number) {
         m_number = number;
     }
     void setCapacity(unsigned int capacity) {
         m_capacity = capacity;
+    }
+
+    void setClassroomType(ClassroomType classroomType) {
+        m_roomType = classroomType;
     }
 
     Classroom& operator=(const Classroom& other) {
@@ -71,6 +78,10 @@ public:
         return true;
     }
 
+    void writeInfo() {
+        getClassroomInfo(*this);
+    }
+
     static unsigned int getCount() {return m_count;}
 
     friend void getClassroomInfo(const Classroom& classroom);
@@ -79,10 +90,35 @@ public:
 unsigned int Classroom::m_count = 0;
 
 inline void getClassroomInfo(const Classroom& classroom) {
-    std::cout << "--Classroom info--\n"
+    std::cout << "--Classroom--\n"
     << "Number: " << classroom.m_number
     << "\nCapacity: " << classroom.m_capacity
-    << "\nRoom Type: " << classroom.m_roomType;
+    << "\nRoom Type: ";
+    if (classroom.m_roomType == LECTURE_CLASSROOM) {
+        std::cout << "LECTURE" << std::endl;
+    }
+    else if (classroom.m_roomType == PRACTICE_CLASSROOM) {
+        std::cout << "PRACTICE" << std::endl;
+    }
+}
+
+inline std::ifstream& operator>>(std::ifstream& ifs, Classroom& classroom) {
+    unsigned int number;
+    unsigned int capacity;
+    unsigned int roomType;
+    ifs >> number >> capacity >> roomType;
+    classroom.setNumber(number);
+    classroom.setCapacity(capacity);
+    classroom.setClassroomType((ClassroomType)roomType);
+    return ifs;
+}
+
+inline std::ofstream& operator<<(std::ofstream& ofs, const Classroom& classroom) {
+    ofs << classroom.getNumber() << std::endl;
+    ofs << classroom.getCapacity() << std::endl;
+    ofs << classroom.getClassroomType() << std::endl;
+
+    return ofs;
 }
 
 #endif //LABS1TO6_CLASSROOM_H
